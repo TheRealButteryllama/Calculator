76e0911c8a6e7597f9471bd322cdd843b72dcae1
DESKTOP-9O4290A\Mike <faulkner.johnfogerty@gmail.com>
2022-05-08 23:04:17 -0400

    Added Functionality

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ console.log("JS");
 
 // pressing function buttons clear the input from the screen so user can add second numbers - fills a variable with the user input
 
-// function stores what numbers user has inputed along with what functions were used. - function puts together the user inputes
-
 // pressing = does the math thing. - function performs the math that the user put in.
 
 let userInput = "";
@@ -17,13 +15,18 @@ let operator = "";
 let userInput2 = "";
 
 var elements = document.getElementsByTagName("input");
+let display = document.getElementById("display");
 
 for (let element of elements) {
-  element.onclick = function (event) {
+  element.onclick = function () {
     var value = element.value;
     var realValue = Number(value);
-    console.log(value, realValue);
-    if (isNaN(realValue)) {
+    if (value == "c") {
+      userInput = "";
+      userInput2 = "";
+      operator = "";
+      display.value = "0";
+    } else if (isNaN(realValue)) {
       if (value == "=" && userInput2.length > 0) {
         var realUserInput = Number(userInput);
         var realUserInput2 = Number(userInput2);
@@ -36,20 +39,22 @@ for (let element of elements) {
         } else if (operator == "/") {
           var result = realUserInput / realUserInput2;
         }
-        console.log(result);
+        display.value = result;
         userInput = "";
         userInput2 = "";
         operator = "";
       } else {
         operator = element.value;
+        display.value = 0;
       }
     } else {
       if (userInput.length > 0 && operator.length > 0) {
         userInput2 += element.value;
+        display.value = userInput2;
       } else {
         userInput += element.value;
+        display.value = userInput;
       }
     }
-    console.log(userInput, operator, userInput2);
   };
 }
